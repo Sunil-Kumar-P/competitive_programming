89377a1c50dbf96c5a7e1e28b5fa48b5f7465cd0
Sunil Kumar P <sunilkpg23@gmail.com>
2023-06-21 13:42:46 +0530

    Create contains_duplicates.java

---
+++ JAVA/contains_duplicates.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class contains_duplicates {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+        try {
+
+            n = Integer.parseInt(reader.readLine());
+
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++) {
+                nums[i] = Integer.parseInt(reader.readLine());
+            }
+
+            SolutionCD solution = new SolutionCD();
+            boolean containsDuplicate = solution.containsDuplicate(nums);
+
+            System.out.println(containsDuplicate);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class SolutionCD {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> uniques = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (uniques.contains(nums[i])) {
+                return true;
+            }
+            uniques.add(nums[i]);
+        }
+        return false;
+    }
+}
