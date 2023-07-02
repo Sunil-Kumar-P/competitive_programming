ed378feabe3f1b2ec57fe6526d0777485e69e95c
Sunil Kumar P <sunilkpg23@gmail.com>
2023-07-02 14:00:57 +0530

    MergeSortedArray leetcode 88

---
+++ JavaScript/MergeSortedArray.js
@@ -0,0 +1,45 @@
+// 88. Merge Sorted Array
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+function merge(nums1, m, nums2, n) {
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p = m + n - 1;
+  
+    while (p1 >= 0 && p2 >= 0) {
+      if (nums1[p1] > nums2[p2]) {
+        nums1[p] = nums1[p1];
+        p1--;
+      } else {
+        nums1[p] = nums2[p2];
+        p2--;
+      }
+      p--;
+    }
+  
+    // Copy remaining elements from nums2 to nums1
+    while (p2 >= 0) {
+      nums1[p] = nums2[p2];
+      p2--;
+      p--;
+    }
+  }
+  
+  const nums1 = [1, 2, 3, 0, 0, 0];
+  const m = 3;
+  const nums2 = [2, 5, 6];
+  const n = 3;
+  
+  merge(nums1, m, nums2, n);
+  // Output: [1, 2, 2, 3, 5, 6]
+  console.log(nums1);
+  
---
+++ Python/mergeSortedArray.py
@@ -0,0 +1,28 @@
+# 88. Merge Sorted Array
+# You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+# Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+# The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+# Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+# Output: [1,2,2,3,5,6]
+# Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+# The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+    p1 = m - 1
+    p2 = n - 1
+    p = m + n - 1
+    while p1 >= 0 and p2 >= 0:
+        if nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+    nums1[:p2 + 1] = nums2[:p2 + 1]
+    print(nums1)
+
+merge([1,2,3,0,0,0],3,[2,5,6],3)
